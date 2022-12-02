91718e5ca4c9c8333287b4ff5771d33543bc6266
nicole-ilustre <nicoleilustre2292@gmail.com>
2022-12-03 09:45:57 +1300

    setup nuxt and vuetify

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+       <v-text-field
+            label="Text Input"
+            outlined
+          ></v-text-field>
+          <v-btn
+        elevation="2"
+        >Submit</v-btn>
+    </v-col>
+ </v-row>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
